479fb5d767879a4be2599b93c84cd3625773d03e
daniel-rivera22 <daniel.rivera@alumnos.upm.es>
2026-05-27 12:30:34 +0200

    add: starred song id list

--- music-hexagonal/src/main/java/com/music/streaming/user/domain/User.java
+++ music-hexagonal/src/main/java/com/music/streaming/user/domain/User.java
@@ -3,6 +3,8 @@ package com.music.streaming.user.domain;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -16,4 +18,7 @@ public class User {
     String username;
     @With
     String email;
+    @Builder.Default
+    @With
+    List<String> starredSongIds = new ArrayList<>();
 }
